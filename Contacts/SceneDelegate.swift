dd77534787bf8908194acea77c76762194a7a31d
Vitalii Yevtushenko <eltiren@eltiren.com>
2021-05-30 20:57:39 +0300

    Created manual window initialization

--- Contacts/SceneDelegate.swift
+++ Contacts/SceneDelegate.swift
@@ -5,6 +5,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
     }
 }
